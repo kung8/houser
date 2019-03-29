859bbca3df6c1a09ee845b38d116379313ad2ec1
Kevin Ung <ung.kevin78@gmail.com>
2019-03-29 08:11:24 -0600

    styled image

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -3,9 +3,11 @@ import React from 'react';
 
 function Header () {
     return(
-        <div>
-            <i className="fas fa-home"></i>
-            <h1 className="logo-formatting">Houser</h1>
+        <div className="header-container">
+            <div className="logo-formatting">
+                <i style={{fontSize:30,color:'white',marginRight:10}} className="fas fa-home"></i>
+                <h1>Houser</h1>
+            </div>
         </div>
     )
 }
